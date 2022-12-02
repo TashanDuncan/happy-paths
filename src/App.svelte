e92b3f56696522997b66276a3b3baa408a4df071
Tay Duncan <Tay.Duncan@autotrader.co.uk>
2022-12-02 14:15:35 +0000

    fix issue with missing logo

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
+  //import svelteLogo from './assets/svelte.svg'
   import Counter from './lib/Counter.svelte'
 </script>
 
@@ -9,7 +9,6 @@
       <img src="/vite.svg" class="logo" alt="Vite Logo" />
     </a>
     <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
   </div>
   <h1>Vite + Svelte</h1>
