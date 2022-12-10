2c41fcd5ee4d69f315c577cd513ada3d84da8375
Tay Duncan <Tay.Duncan@autotrader.co.uk>
2022-12-10 12:22:21 +0000

    add in game clock

--- src/lib/Time.svelte
+++ src/lib/Time.svelte
@@ -1,9 +1,34 @@
 <script lang="ts">
+  let mins = 0;
+  let hours = 9;
 
+  function timer() {
+    mins += 10;
+    if (mins === 60) {
+      mins = 0;
+      hours++;
+    }
+  }
+
+  $: if(hours === 18){
+    alert('GAME OVER! We will put a game over overlay here:) ')
+  }
+  //currently set to add 10 mins each second. should probably slow this down in live
+  setInterval(timer, 1000);
+
+  $: time = `${hours}:${mins === 0 ? "00" : mins}`;
 </script>
 
-<div class="box date"></div>
+<div class="box date">
+  <h1>Monday</h1>
+  <span>{time}</span>
+</div>
 
 <style>
-
+  .date{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
 </style>
