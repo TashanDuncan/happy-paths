5e632fd51b200f68928c1f73c5e9cb55aa05dbb7
Shu Wang <shu.wang@autotrader.co.uk>
2022-12-13 22:11:51 +0000

    Add more events chain to gameConfig.ts

--- src/lib/gameConfig.ts
+++ src/lib/gameConfig.ts
@@ -41,40 +41,135 @@ export const gameConfig: GameConfig[] = [
         scenario: "Start",
         prompt: "What to do first?",
         options: [
-            {option: "Work on ticket", energy: -10, scenarioId: "debugging-monday", timeIncrease: 10},
+            {option: "Work on ticket", energy: -10, scenarioId: "ticket-monday", timeIncrease: 10},
             {option: "Check email", energy: -10, scenarioId: "phishing-monday", timeIncrease: 60},
-            {option: "Go Home", energy: -100, scenarioId: "home-monday", timeIncrease: 120},
+            {option: "Go Home", energy: -100, scenarioId: "home-monday", timeIncrease: 0},
             {option: "Poo", energy: +10, scenarioId: "bathroom-monday", timeIncrease: 60},
-            {option: "Fire?", energy: -10, scenarioId: "fire-monday", timeIncrease: 60}
+        ],
+        random: false
+    },
+    {
+        id: "ticket-monday",
+        scenario: "Ticket",
+        prompt: "The legacy codebase you're working on is full of bugs! Fix as many you can before lunch.",
+        options: [
+            {option: "Start!", energy: 0, scenarioId: "debugging-game-monday", timeIncrease: 0 },
+        ],
+        random: false
+    },
+    {
+        id: "debugging-game-monday",
+        scenario: "Debugging game",
+        prompt: "somehow implement mini game here",
+        options: [
+            {option: "Next", energy: -40, scenarioId: "lunch-monday", timeIncrease: 120 },
         ],
         random: false
     },
     {
         id: "phishing-monday",
         scenario: "Phishing",
-        prompt: "You open outlook and find a new email from official@appl3.com: ",
+        prompt: `You open outlook and find a new email from official@appl3.com:
+        Dear Customer,
+Plz send bank info for brand new iPhone 100.
+Thank,
+Official Appl3`,
         options: [
             {option: "I've been wanting a new iPhone for ages! Click link.", energy: -10, scenarioId: "phished-monday", timeIncrease: 30},
-            {option: "Hmm very sus. Report and forward to IT.", energy: -10, scenarioId: "meeting-monday", timeIncrease: 30},
+            {option: "Hmm very sus. Report and forward to IT.", energy: -10, scenarioId: "not-phished-monday", timeIncrease: 30},
             {option: "Oh no, your laptop must have been infected by a deadly virus. Burn it asap.", energy: -50, scenarioId: "destruction-monday", timeIncrease: 60}
         ],
         random: false
     },
     {
-        id: "slack-monday",
-        scenario: "Slack",
-        prompt: "something something",
+        id: "not-phished-monday",
+        scenario: "Not Phished",
+        prompt: "Turns out it was all a test! And you passed. Generic automotive advertising company is very proud of you. Yay.",
         options: [
-            {option: "Work on story", energy: -20, scenarioId: "project", timeIncrease: 200}],
+            {option: "aw :(", energy: 0, scenarioId: "meeting-monday", timeIncrease: 0 }
+        ],
+        random: false
+    },
+    {
+        id: "phished-monday",
+        scenario: "Phished",
+        prompt: "Turns out it was all a test! And you failed. Generic automotive advertising company is very disappointed in you. Boo.",
+        options: [
+            {option: "aw :(", energy: 0, scenarioId: "meeting-monday", timeIncrease: 0 }
+        ],
+        random: false
+    },
+    {
+        id: "destruction-monday",
+        scenario: "Destruction",
+        prompt: "Um...no. The laptop was not possessed and you just destructed company property. Shame on you.",
+        options: [
+            {option: "Are you sure about that?", energy: 0, scenarioId: "meeting-monday", timeIncrease: 0 },
+            {option: "Oh no, you're possessed too!", energy: 0, scenarioId: "meeting-monday", timeIncrease: 0 }
+        ],
+        random: false
+    },
+    {
+        id: "meeting-monday",
+        scenario: "Meeting",
+        prompt: "New to the job, you just realised all the meeting rooms are named after cars... Your next meeting is in 10 mins, can you remember which room it's in?",
+        options: [
+            {option: "Give it a go!", energy: -10, scenarioId: "memory-game-monday", timeIncrease: 0 },
+        ],
+        random: false
+    },
+    {
+        id: "memory-game-monday",
+        scenario: "Memory game",
+        prompt: "somehow implement mini game here",
+        options: [
+            {option: "Next", energy: 0, scenarioId: "lunch-monday", timeIncrease: 0 },
+        ],
+        random: false
+    },
+    {
+        id: "lunch-monday",
+        scenario: "Lunch",
+        prompt: "Good meeting! Now what should we get for lunch?",
+        options: [
+            {option: "Sainsbury's meal deal", energy: +20, scenarioId: "fire-monday", timeIncrease: 60 },
+            {option: "PaxterStorey", energy: +40, scenarioId: "fire-monday", timeIncrease: 60 },
+            {option: "It's treat Monday, let's go somewhere nice.", energy: +50, scenarioId: "fire-monday", timeIncrease: 60 },
+        ],
         random: false
     },
     {
         id: "fire-monday",
         scenario: "Fire",
-        prompt: "The fire alarm just went off and everything's evacuating!",
+        prompt: "That was a nice meal, let's get back to work. Wait, what's that sound? The fire alarm just went off and everyone's evacuating!",
         options: [
-            {option: "Follow everyone!", energy: -20, scenarioId: "evacuated", timeIncrease: 100},
-            {option: "It's probably a drill, even though those never takes place on Mondays...Chill.", energy: -100, scenarioId: "fiery_death", timeIncrease: 0}],
-            random: false
+            {option: "Follow everyone!", energy: -20, scenarioId: "evacuated-monday", timeIncrease: 100},
+            {option: "It's probably a drill, even though those never takes place on Mondays...Chill.", energy: -100, scenarioId: "fiery-death-monday", timeIncrease: 0}],
+        random: false
+    },
+    {
+        id: "evacuated-monday",
+        scenario: "Evacutaed",
+        prompt: "Turns out there really was a fire! Apparently someone tried to burn up a possessed laptop :/",
+        options: [
+            {option: "Head back to the office", energy: 0, scenarioId: "slack-monday", timeIncrease: 0 }],
+        random: false
+    },
+    {
+        id: "slack-monday",
+        scenario: "Slack",
+        prompt: "Someone just posted on slack. Your company's test driver just called off sick, the company is asking if you want to help them test out a new vehicle!",
+        options: [
+            {option: "Heck yeah!", energy: 0, scenarioId: "driving-game-monday", timeIncrease: 120}
+        ],
+        random:false
+    },
+    {
+        id: "driving-game-monday",
+        scenario: "Driving game",
+        prompt: "Implement mini game here",
+        options: [
+        ],
+        random:false
     }
 ];
