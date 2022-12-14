732daf35d0d48586484a23792d7cb78307522d95
Tay Duncan <Tay.Duncan@autotrader.co.uk>
2022-12-14 14:31:53 +0000

    hide Music button on GameOver

--- src/lib/Music.svelte
+++ src/lib/Music.svelte
@@ -1,38 +1,42 @@
 <script lang="ts">
-  export let currentScenarioType
-$: isPlaying = false
-  let backgroundMusic = new Audio('sounds/we-wish-you-a-merry-christmas-126685.mp3')
-  let gameOverSound = new Audio('sounds/failure-4-with-gong-69906.mp3')
+  export let currentScenarioType;
+  $: isPlaying = false;
+  $: isShowing = true;
+  let backgroundMusic = new Audio(
+    "sounds/we-wish-you-a-merry-christmas-126685.mp3"
+  );
+  let gameOverSound = new Audio("sounds/failure-4-with-gong-69906.mp3");
 
-  $: if(currentScenarioType === 'game-over'){
-    backgroundMusic.pause()
-    gameOverSound.play()
-}
-  backgroundMusic.loop = true;
-  function toggleMusic(){
-    if(!backgroundMusic.paused ) {
-      backgroundMusic.pause()
-      isPlaying = false
-    } else {
-      backgroundMusic.play()
-      isPlaying = true
-    }
-
-  
+  $: if (currentScenarioType === "game-over") {
+    isShowing = false
+    backgroundMusic.pause();
+    gameOverSound.play();
   }
-
+  backgroundMusic.loop = true;
+  function toggleMusic() {
+    if (!backgroundMusic.paused) {
+      backgroundMusic.pause();
+      isPlaying = false;
+    } else {
+      backgroundMusic.play();
+      isPlaying = true;
+    }
+  }
 </script>
-<div></div>
-<button class="sound" on:click={toggleMusic}>
-  {#if isPlaying}
-  <i class="fas fa-volume-up fa-3x" ></i>
-  {:else}
-  <i class="fas fa-volume-mute fa-3x" ></i>
-  {/if}
+
+<div />
+{#if isShowing}
+  <button class="sound" on:click={toggleMusic}>
+    {#if isPlaying}
+      <i class="fas fa-volume-up fa-3x" />
+    {:else}
+      <i class="fas fa-volume-mute fa-3x" />
+    {/if}
   </button>
+{/if}
 
 <style>
-  .sound{
+  .sound {
     position: absolute;
     right: 1%;
     top: 1%;
